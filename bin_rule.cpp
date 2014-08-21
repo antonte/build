8bded9f27def553b00389e20e7aee10b3a2ed492
Anton Te <te.anton@gmail.com>
2014-08-20 18:12:10 -0700

    - fix the bug when if directory name is same as one of switches in build.cfg, this switch will not be used during the compilation - fix the bug with ever growing config file for internal libraries

--- bin_rule.cpp
+++ bin_rule.cpp
@@ -14,117 +14,123 @@
 
 BinRule::BinRule()
 {
-    create_directory("tmp");
-    if (!archive)
-        target_ = filename(current_path());
-    else
-        target_ = "lib" + filename(current_path()) + ".a";
-    std::unique_ptr<DIR, decltype(&closedir)> d(opendir("."), &closedir);
-    while (auto de = readdir(d.get()))
-        if (de->d_type == DT_REG && extension(de->d_name) == "cpp")
-            dependencies_.push_back(std::unique_ptr<ObjRule>(new ObjRule(de->d_name)));
+  create_directory("tmp");
+  if (!archive)
+    target_ = filename(current_path());
+  else
+    target_ = "lib" + filename(current_path()) + ".a";
+  std::unique_ptr<DIR, decltype(&closedir)> d(opendir("."), &closedir);
+  while (auto de = readdir(d.get()))
+    if (de->d_type == DT_REG && extension(de->d_name) == "cpp")
+      dependencies_.push_back(std::unique_ptr<ObjRule>(new ObjRule(de->d_name)));
 }
 
 void BinRule::internalResolve()
 {
-    std::vector<std::string> libs;
-    if (!archive)
+  std::vector<std::string> libs;
+  if (!archive)
+  {
+    std::ostringstream cmd;
+    cmd << "g++ -march=native -gdwarf-3 ";
+    if (exists(libDir))
+      cmd << "-L" << libDir << " ";
+    for (auto &d: dependencies_)
     {
-        std::ostringstream cmd;
-        cmd << "g++ -march=native -gdwarf-3 ";
-        if (exists(libDir))
-            cmd << "-L" << libDir << " ";
-        for (auto &d: dependencies_)
-        {
-            cmd << d->target() << " ";
-            std::ifstream f(d->target() + ".inc");
-            while (f.good())
-            {
-                std::string s;
-                f >> s;
-                auto l = inc2lib[s];
-                libs.insert(end(libs), begin(l), end(l));
-            }
-        }
-        std::set<std::string> uniqueLibs;
-        for (auto &i: libs)
-            if (i != target_)
-                if (uniqueLibs.find(i) == std::end(uniqueLibs))
-                {
-                    cmd << "-l" << i << " ";
-                    uniqueLibs.insert(i);
-                }
-        cmd << "-o " << target_;
-        std::cout << cmd.str() << std::endl;
-        auto res = system(cmd.str().c_str());
-        if (res != 0)
-            throw std::runtime_error("Linking error");
+      cmd << d->target() << " ";
+      std::ifstream f(d->target() + ".inc");
+      while (f.good())
+      {
+        std::string s;
+        f >> s;
+        auto l = inc2lib[s];
+        libs.insert(end(libs), begin(l), end(l));
+      }
     }
-    else
+    std::set<std::string> uniqueLibs;
+    for (auto &i: libs)
+      if (uniqueLibs.find(i) == std::end(uniqueLibs))
+      {
+        cmd << "-l" << i << " ";
+        uniqueLibs.insert(i);
+      }
+    cmd << "-o " << target_;
+    std::cout << cmd.str() << std::endl;
+    auto res = system(cmd.str().c_str());
+    if (res != 0)
+      throw std::runtime_error("Linking error");
+  }
+  else
+  {
     {
+      std::ostringstream cmd;
+      cmd << "ar r ";
+      cmd << target_;
+      std::set<std::string> uniqueLibs;
+      for (auto &d: dependencies_)
+      {
+        cmd << " " << d->target();
+        std::ifstream f(d->target() + ".inc");
+        while (f.good())
         {
-            std::ostringstream cmd;
-            cmd << "ar r ";
-            cmd << target_;
-            for (auto &d: dependencies_)
+          std::string s;
+          f >> s;
+          for (auto lib: inc2lib[s])
+            if (uniqueLibs.find(lib) == std::end(uniqueLibs))
             {
-                cmd << " " << d->target();
-                std::ifstream f(d->target() + ".inc");
-                while (f.good())
-                {
-                    std::string s;
-                    f >> s;
-                    auto l = inc2lib[s];
-                    libs.insert(end(libs), begin(l), end(l));
-                }
+              libs.push_back(lib);
+              uniqueLibs.insert(lib);
             }
-            std::cout << cmd.str() << std::endl;
-            auto res = system(cmd.str().c_str());
-            if (res != 0)
-                throw std::runtime_error("Linking error");
         }
-        {
-            std::ostringstream cmd;
-            cmd << "mkdir -p " << incDir << "/a/";
-            auto res = system(cmd.str().c_str());
-            if (res != 0)
-                throw std::runtime_error("Cannot create a directory ");
-        }
-        {
-            std::ostringstream cmd;
-            cmd << "mkdir -p " << libDir;
-            auto res = system(cmd.str().c_str());
-            if (res != 0)
-                throw std::runtime_error("Cannot create a directory ");
-        }
-        {
-            std::ostringstream cmd;
-            cmd << "mkdir -p " << cfgDir;
-            auto res = system(cmd.str().c_str());
-            if (res != 0)
-                throw std::runtime_error("Cannot create a directory ");
-        }
+      }
+      std::cout << cmd.str() << std::endl;
+      auto res = system(cmd.str().c_str());
+      if (res != 0)
+        throw std::runtime_error("Linking error");
+    }
+    {
+      std::ostringstream cmd;
+      cmd << "mkdir -p " << incDir << "/a/";
+      auto res = system(cmd.str().c_str());
+      if (res != 0)
+        throw std::runtime_error("Cannot create a directory ");
+    }
+    {
+      std::ostringstream cmd;
+      cmd << "mkdir -p " << libDir;
+      auto res = system(cmd.str().c_str());
+      if (res != 0)
+        throw std::runtime_error("Cannot create a directory ");
+    }
+    {
+      std::ostringstream cmd;
+      cmd << "mkdir -p " << cfgDir;
+      auto res = system(cmd.str().c_str());
+      if (res != 0)
+        throw std::runtime_error("Cannot create a directory ");
+    }
         
-        std::ofstream f(cfgDir + "/" + target_ + ".cfg");
-        std::unique_ptr<DIR, decltype(&closedir)> d(opendir("."), &closedir);
-        while (auto de = readdir(d.get()))
-            if (de->d_type == DT_REG && extension(de->d_name) == "hpp")
-            {
-                std::ostringstream cmd;
-                cmd << "ln -sf " << absolutePath(de->d_name) << " " << incDir << "/a/" << de->d_name;
-                std::cout << cmd.str() << std::endl;
-                auto res = system(cmd.str().c_str());
-                if (res != 0)
-                    throw std::runtime_error("Cannot link a header file");
-                f << de->d_name << " " << filename(current_path()) << std::endl;
-                for (auto &i: libs)
-                    f << de->d_name << " " << i << std::endl;
-            }
+    std::ofstream f(cfgDir + "/" + target_ + ".cfg");
+    std::unique_ptr<DIR, decltype(&closedir)> d(opendir("."), &closedir);
+    while (auto de = readdir(d.get()))
+      if (de->d_type == DT_REG && extension(de->d_name) == "hpp")
+      {
         std::ostringstream cmd;
-        cmd << "ln -sf " << absolutePath(target_) << " " << libDir << "/" << target_;
+        cmd << "ln -sf " << absolutePath(de->d_name) << " " << incDir << "/a/" << de->d_name;
         std::cout << cmd.str() << std::endl;
         auto res = system(cmd.str().c_str());
         if (res != 0)
-            throw std::runtime_error("Cannot link a static library (archive) file");
-    }
+          throw std::runtime_error("Cannot link a header file");
+        auto tmp = filename(current_path());
+        f << de->d_name << " " << tmp << std::endl;
+        for (auto &i: libs)
+          if (i != tmp)
+            f << de->d_name << " " << i << std::endl;
+      }
+    std::ostringstream cmd;
+    cmd << "ln -sf " << absolutePath(target_) << " " << libDir << "/" << target_;
+    std::cout << cmd.str() << std::endl;
+    auto res = system(cmd.str().c_str());
+    if (res != 0)
+      throw std::runtime_error("Cannot link a static library (archive) file");
+  }
 }
